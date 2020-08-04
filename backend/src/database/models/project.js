58030d22763edbe3a91b363e4ff2ddb87ec34251
Ruben-schwarz <61813618+Ruben-schwarz@users.noreply.github.com>
2020-08-04 15:19:58 -0400

    Update project.js

--- backend/src/database/models/project.js
+++ backend/src/database/models/project.js
@@ -5,7 +5,9 @@ const projectSchema = new Schema({
   owner: { type: Object, unique: false, required: true },
   name: { type: String, unique: true, required: true },
   tasks: { type: Array, unique: false, required: true },
-  completed: { type: Boolean, unique: false, default: false },
-});
+  completed: { type: Boolean, unique: false, default: false },// basically just here for the future 
+})                                                           // 90% sure i didnt add the "completed"
+                                                              // feature
+                                                           // everything else is pretty standard
 
 module.exports = mongoose.model("Project", projectSchema);
